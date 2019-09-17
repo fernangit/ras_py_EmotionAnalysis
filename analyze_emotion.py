ee1423674b7763593be06e8c22e57d2ac83ca439
fernangit <morio.tsunaki@gmail.com>
2019-09-17 22:51:04 +0900

    add files

--- analyze_emotion.py
+++ analyze_emotion.py
@@ -4,7 +4,8 @@ from keras.preprocessing import image
 import numpy as np
 import random
 
-rgb_image = './images/mika_74.jpg'
+rgb_image = './images/face.jpg'
+#rgb_image = './images/mika_74.jpg'
 #rgb_image = './images/mika_110.jpg'
 #rgb_image = './images/rika_67.jpg'
 #rgb_image = './images/rika_132.jpg'
@@ -24,25 +25,13 @@ emotion_phrase =({
 6:["自然体","いい感じ","ナチュラル"]
 })
 
-model = load_model(emotion_model_path, compile=False)
-
 #感情予測
-def emotion():
+def emotion(model):
     load_img = image.load_img(rgb_image, grayscale=True , target_size=(64, 64))
     x = image.img_to_array(load_img)
     x = np.expand_dims(x, axis=0) / 255
 
     predict = model.predict(x)
-    return predict
-
-def main():
-    #配列初期化
-    a = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.4])
-    predicts = np.tile(a, (10, 1))
-    print(predicts)
-
-    #感情予測
-    predict = emotion()
     print(predict)
     emotion_label_arg = np.argmax(predict)
     emotion_text = emotion_labels[emotion_label_arg]
@@ -51,8 +40,11 @@ def main():
     print(emotion_text,emotion_ratio)
     print('{}ですね。'.format(emotion_phrase_rand))
 
+    return predict
+
+# 平均化
+def mean_emotion(predict, predicts):
     #結果挿入
-    np.insert(predicts, 0, 1, axis=0)
     predicts = np.vstack((predicts, predict))
     predicts = np.delete(predicts, 0, axis=0)
 #    print(predicts)
@@ -72,6 +64,22 @@ def main():
     emotion_phrase_rand = random.choice(emotion_phrase[emotion_label_arg])
 #    print(emotion_text,emotion_ratio)
     print('{}'.format(emotion_phrase_rand))
+
+    return predicts
+
+def main():
+    # モデルロード
+    model = load_model(emotion_model_path, compile=False)
+    #配列初期化
+    a = np.array([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.4])
+    predicts = np.tile(a, (10, 1))
+    print(predicts)
+
+    #感情分析
+    predict = emotion(model)
+
+    #平均算出
+    predicts = mean_emotion(predict, predicts)
 
 if __name__ == '__main__':
     main()
